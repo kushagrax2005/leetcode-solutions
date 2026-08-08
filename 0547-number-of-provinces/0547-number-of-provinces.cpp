4b761388b87ee70ec4db8277782da9023d78ffa1
kushagrax2005 <kushagra14official@gmail.com>
2026-08-08 19:42:28 +0530

    Time: 0 ms (100%), Space: 19.9 MB (47.45%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -13,19 +13,48 @@ public:
         }
     }
     int findCircleNum(vector<vector<int>>& is) {
+        // int n=is.size();
+        // int maxi=n;
+        // vector<int>vis(n,0);
+        // for(int i=0;i<n;i++)
+        // {
+        // if(!vis[i])
+        // {
+        // vector<int>ls;
+        // dfs(i,ls,vis,is);
+        // int a=ls.size();
+        // maxi=maxi-a+1;
+        // }
+        // }
+        // return maxi;
+
         int n=is.size();
-        int maxi=n;
+        // int i=0;
+        int maxi=0;
         vector<int>vis(n,0);
         for(int i=0;i<n;i++)
         {
-        if(!vis[i])
-        {
-        vector<int>ls;
-        dfs(i,ls,vis,is);
-        int a=ls.size();
-        maxi=maxi-a+1;
+            if(vis[i])continue;
+            vector<int>ls;
+            queue<int>q;
+            q.push(i);
+            vis[i]=1;
+            while(!q.empty())
+            {
+                int node=q.front();
+                q.pop();
+                ls.push_back(node);
+                for(int j=0;j<n;j++)
+                {
+                    if(node!=j&&!vis[j]&&is[node][j])
+                    {
+                        vis[j]=1;
+                        q.push(j);
+                    }
+                }
+            }
+            maxi=maxi-ls.size()+1;
         }
-        }
-        return maxi;
+        return maxi+n;
     }
 };
