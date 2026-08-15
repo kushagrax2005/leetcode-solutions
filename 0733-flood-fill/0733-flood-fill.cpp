2fe794f96717d4c4ad4a0e696ebc7b14079b88a8
kushagrax2005 <kushagra14official@gmail.com>
2026-08-15 15:55:35 +0530

    Time: 0 ms (100%), Space: 22.1 MB (8.13%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,31 +1,50 @@
 class Solution {
 public:
+    void dfs(int i,int j,vector<vector<int>>& image,vector<int>&vis,int color,int a)
+    {
+        vis[50*i+j]=1;
+        image[i][j]=color;
+        vector<pair<int,int>>v={{-1,0},{0,-1},{1,0},{0,1}};
+        for(int k=0;k<4;k++)
+        {
+            int x=i+v[k].first;
+            int y=j+v[k].second;
+            if(x>=0&&x<image.size()&&y>=0&&y<image[0].size()&&image[x][y]==a&&!vis[50*x+y])
+            {
+               dfs(x,y,image,vis,color,a);
+            }
+        }
+    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int m=image.size();
         int n=image[0].size();
         vector<int>vis(50*50,0);
-        queue<int>q;
-        q.push(50*sr+sc);
-        vis[50*sr+sc]=1;
-        int a=image[sr][sc];
-        image[sr][sc]=color;
         vector<pair<int,int>>v={{-1,0},{0,-1},{1,0},{0,1}};
-        while(!q.empty())
-        {
-            int node=q.front();
-            q.pop();
-            for(int j=0;j<4;j++)
-            {
-                int x=node/50+v[j].first;
-                int y=node%50+v[j].second;
-                if(x>=0&&x<m&&y>=0&&y<n&&image[x][y]==a&&!vis[50*x+y])
-                {
-                    vis[50*x+y]=1;
-                    image[x][y]=color;
-                    q.push(50*x+y);
-                }
-            }
-        }
+        int a=image[sr][sc];
+
+        dfs(sr,sc,image,vis,color,a);
+
+
+        // queue<int>q;
+        // q.push(50*sr+sc);
+        // vis[50*sr+sc]=1;
+        // image[sr][sc]=color;
+        // while(!q.empty())
+        // {
+        //     int node=q.front();
+        //     q.pop();
+        //     for(int j=0;j<4;j++)
+        //     {
+        //         int x=node/50+v[j].first;
+        //         int y=node%50+v[j].second;
+        //         if(x>=0&&x<m&&y>=0&&y<n&&image[x][y]==a&&!vis[50*x+y])
+        //         {
+        //             vis[50*x+y]=1;
+        //             image[x][y]=color;
+        //             q.push(50*x+y);
+        //         }
+        //     }
+        // }
         return image;
     }
 };
