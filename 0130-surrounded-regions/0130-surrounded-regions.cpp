22aa0209da36af18f4779aeebbeb2aeb3bc881e8
kushagrax2005 <kushagra14official@gmail.com>
2026-08-24 20:47:11 +0530

    Time: 0 ms (100%), Space: 14.5 MB (59.52%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -41,15 +41,114 @@ public:
          int m=b.size();
          int n=b[0].size();
         //  unordered_map<int,int>mm;
+
          for(int j=0;j<n;j++)
          {
-            bfs(0,j,b);
-            bfs(m-1,j,b);
+            int i=0;
+            if(b[i][j] != 'O') continue;
+            int node=i*b[0].size()+j;
+        queue<int>q;
+        q.push(node);
+        b[i][j]='0';
+        vector<pair<int,int>>v={{0,1},{0,-1},{1,0},{-1,0}};
+        while(!q.empty())
+        {
+            node=q.front();
+            q.pop();
+            int x=node/b[0].size();
+            int y=node%b[0].size();
+            for(int k=0;k<4;k++)
+            {
+                int aa=x+v[k].first;
+                int bb=y+v[k].second;
+                if(aa>=0&&aa<b.size()&&bb>=0&&bb<b[0].size()&&b[aa][bb]=='O')
+                {
+                    q.push(aa*b[0].size()+bb);
+                    b[aa][bb]='0';
+                }
+            }
+        }
+         }
+         for(int j=0;j<n;j++)
+         {
+            int i=m-1;
+            if(b[i][j] != 'O') continue;
+            int node=i*b[0].size()+j;
+        queue<int>q;
+        q.push(node);
+        b[i][j]='0';
+        vector<pair<int,int>>v={{0,1},{0,-1},{1,0},{-1,0}};
+        while(!q.empty())
+        {
+            node=q.front();
+            q.pop();
+            int x=node/b[0].size();
+            int y=node%b[0].size();
+            for(int k=0;k<4;k++)
+            {
+                int aa=x+v[k].first;
+                int bb=y+v[k].second;
+                if(aa>=0&&aa<b.size()&&bb>=0&&bb<b[0].size()&&b[aa][bb]=='O')
+                {
+                    q.push(aa*b[0].size()+bb);
+                    b[aa][bb]='0';
+                }
+            }
+        }
          }
          for(int i=1;i<m-1;i++)
          {
-            bfs(i,0,b);
-            bfs(i,n-1,b);
+            int j=0;
+            if(b[i][j] != 'O') continue;
+           int node=i*b[0].size()+j;
+        queue<int>q;
+        q.push(node);
+        b[i][j]='0';
+        vector<pair<int,int>>v={{0,1},{0,-1},{1,0},{-1,0}};
+        while(!q.empty())
+        {
+            node=q.front();
+            q.pop();
+            int x=node/b[0].size();
+            int y=node%b[0].size();
+            for(int k=0;k<4;k++)
+            {
+                int aa=x+v[k].first;
+                int bb=y+v[k].second;
+                if(aa>=0&&aa<b.size()&&bb>=0&&bb<b[0].size()&&b[aa][bb]=='O')
+                {
+                    q.push(aa*b[0].size()+bb);
+                    b[aa][bb]='0';
+                }
+            }
+        }
+         }
+         for(int i=1;i<m-1;i++)
+         {
+            int j=n-1;
+            if(b[i][j] != 'O') continue;
+            int node=i*b[0].size()+j;
+        queue<int>q;
+        q.push(node);
+        b[i][j]='0';
+        vector<pair<int,int>>v={{0,1},{0,-1},{1,0},{-1,0}};
+        while(!q.empty())
+        {
+            node=q.front();
+            q.pop();
+            int x=node/b[0].size();
+            int y=node%b[0].size();
+            for(int k=0;k<4;k++)
+            {
+                int aa=x+v[k].first;
+                int bb=y+v[k].second;
+                if(aa>=0&&aa<b.size()&&bb>=0&&bb<b[0].size()&&b[aa][bb]=='O')
+                {
+                    q.push(aa*b[0].size()+bb);
+                    b[aa][bb]='0';
+                }
+            }
+        }
          }
          for(int i=0;i<m;i++)
          {
