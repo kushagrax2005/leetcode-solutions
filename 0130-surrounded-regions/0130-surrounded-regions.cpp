7b66f36bafe44b124e836d94329991388fcfc0b5
kushagrax2005 <kushagra14official@gmail.com>
2026-08-24 17:10:58 +0530

    Time: 6 ms (6.72%), Space: 19.9 MB (5.08%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,47 +1,49 @@
 class Solution {
 public:
-    void dfs(int i,int j,unordered_map<int,int>&mm,vector<vector<char>>& b)
+    void dfs(int i,int j,vector<vector<char>>& b)
     {
-        if(i<0||i>=b.size()||j<0||j>=b[0].size()||mm.count(i*b[0].size()+j))return;
-        if(b[i][j]=='O')mm[i*b[0].size()+j]++;
+        if(i<0||i>=b.size()||j<0||j>=b[0].size())return;
+        // if(b[i][j]=='O')mm[i*b[0].size()+j]++;
+        if(b[i][j]=='O')b[i][j]='0';
         else return;
         vector<pair<int,int>>v={{0,1},{0,-1},{1,0},{-1,0}};
         for(int k=0;k<4;k++)
         {
-            dfs(i+v[k].first,j+v[k].second,mm,b);
+            dfs(i+v[k].first,j+v[k].second,b);
         }
     }
     void solve(vector<vector<char>>& b) {
          int m=b.size();
          int n=b[0].size();
-         unordered_map<int,int>mm;
+        //  unordered_map<int,int>mm;
          for(int j=0;j<n;j++)
          {
-            if(!mm.count(0*n+j))
+            if(b[0][j]=='O')
             {
-                dfs(0,j,mm,b);
+                dfs(0,j,b);
             }
-            if(!mm.count((m-1)*n+j))
+            if(b[m-1][j]=='O')
             {
-                dfs(m-1,j,mm,b);
+                dfs(m-1,j,b);
             }
          }
          for(int i=1;i<m-1;i++)
          {
-            if(!mm.count(i*n+0))
+            if(b[i][0]=='O')
             {
-                dfs(i,0,mm,b);
+                dfs(i,0,b);
             }
-            if(!mm.count(i*n+n-1))
+            if(b[i][n-1]=='O')
             {
-                dfs(i,n-1,mm,b);
+                dfs(i,n-1,b);
             }
          }
          for(int i=0;i<m;i++)
          {
             for(int j=0;j<n;j++)
             {
-                if(b[i][j]=='O'&&!mm.count(i*n+j))b[i][j]='X';
+                if(b[i][j]=='O')b[i][j]='X';
+                else if(b[i][j]=='0')b[i][j]='O';
             }
          }
          return ;
