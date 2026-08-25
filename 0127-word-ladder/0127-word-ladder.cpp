92902e7fa550ee7beac084bd11660f4622e86cf6
kushagrax2005 <kushagra14official@gmail.com>
2026-08-25 22:01:31 +0530

    Time: 131 ms (30.7%), Space: 29.1 MB (13.46%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,48 +1,82 @@
 class Solution {
 public:
+    void dfs(string node,string beginWord,unordered_map<string,vector<string>>&con,vector<vector<string>>&vec,vector<string>&v)
+    {
+        if(node==beginWord)
+        {
+            vector<string> temp = v;
+            reverse(temp.begin(), temp.end());
+            vec.push_back(temp);
+            return;
+        }
+        for(string it:con[node])
+        {
+            v.push_back(it);
+            dfs(it,beginWord,con,vec,v);
+            v.pop_back();
+        }
+    }
     int ladderLength(string beginWord, string endWord, vector<string>& w) {
-        unordered_map<string,int>m;
         int n=w.size();
+        unordered_map<string,int>m;
         for(int i=0;i<n;i++)
         {
             m[w[i]]=i;
         }
-        if(!m.count(endWord))return 0;
-        vector<int>vis(n,0);
-        string node=beginWord;
-        int ans=1;
+        if(!m.count(endWord))return {};
         queue<string>q;
+        string node=beginWord;
         q.push(node);
-        // vis[m[node]]=1;
-        
+        // vector<int>vis(n,0);
+        unordered_map<string,int>level;
+        bool flag=false;
+        unordered_map<string,vector<string>>con;
         while(!q.empty())
         {
-            int sq=q.size();
-            while(sq--)
-            {
             node=q.front();
             q.pop();
-            if(node==endWord)return ans;
+            if(node==endWord)flag=true;
             for(int i=0;i<w[0].size();i++)
             {
                 for(int j=0;j<26;j++)
                 {
                     string s=node;
                     char a='a'+j;
-                    if(a==s[i])continue;
+                    if(s[i]==a)continue;
                     s[i]=a;
-                    if(m.count(s)&&!vis[m[s]])
+                    // if(m.count(s) && !vis[m[s]])
+                    // {
+                    //     q.push(s);
+                    //     vis[m[s]]=1;
+                    //     con[s].push_back(node);
+                    // }
+                    if(m.count(s))
                     {
-                        q.push(s);
-                        vis[m[s]]=1;
+                        if(!level.count(s))
+                        {
+                            level[s] = level[node] + 1;
+                            q.push(s);
+                            con[s].push_back(node);
+                        }
+                        else if(level[s] == level[node] + 1)
+                        {
+                            con[s].push_back(node);
+                        }
                     }
                 }
             }
-
-            }
-            ans++;
         }
-        return 0;
-
+        if(!flag)return {};
+        int k=1;
+        node=endWord;
+        // while(node!=beginWord)
+        // {
+        //     int aa=m[node]
+        // }
+        vector<string>v;
+        v.push_back(node);
+        vector<vector<string>>vec;
+        dfs(node,beginWord,con,vec,v);
+        return level[endWord]+1;
     }
 };
