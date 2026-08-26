ea38e6f084d58569563227559a675f606905d0fc
kushagrax2005 <kushagra14official@gmail.com>
2026-08-26 17:56:18 +0530

    Time: 51 ms (8.1%), Space: 36.8 MB (5.04%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
+    void dfs(int i,int j,vector<int>&vis,vector<vector<char>>&grid)
+    {
+        int m=grid.size();
+        int n=grid[0].size();
+        int node=i*n+j;
+        if(i<0||i>=m||j<0||j>=n||grid[i][j]=='0') return ;
+        if(grid[i][j]=='1' && !vis[node])
+        vis[node]=1;
+        else return;
+        vector<pair<int,int>>v={{0,1},{0,-1},{-1,0},{1,0}};
+        for(int k=0;k<4;k++)
+        {
+            int aa=i+v[k].first;
+            int bb=j+v[k].second;
+            dfs(aa,bb,vis,grid);
+        }
+    }
     int numIslands(vector<vector<char>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        vector<int>vis(m*n,0);
-        queue<int>q;
-        vector<pair<int,int>>v={{-1,0},{0,-1},{0,1},{1,0}};
         int ans=0;
-        for(int i=0;i<m;i++)
+        vector<int>vis(m*n,0);
+        for(int i=0;i<grid.size();i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<grid[0].size();j++)
             {
-                if(vis[i*n+j] || grid[i][j]=='0') continue;
-                q.push(i*n+j);
-                vis[i*n+j]=1;
-                ans++;
-                while(!q.empty())
+                if(grid[i][j]=='1'&&!vis[i*n+j])
                 {
-                    int node=q.front();
-                    q.pop();
-                    int x=node/n;
-                    int y=node%n;
-                    for(int jj=0;jj<4;jj++)
-                    {
-                        int aa=x+v[jj].first;
-                        int bb=y+v[jj].second;
-                        if(aa>=0 && aa<m && bb>=0 && bb<n && grid[aa][bb]=='1' && !vis[aa*n+bb])
-                        {
-                            q.push(aa*n+bb);
-                            vis[aa*n+bb]=1;
-                        }
-                    }
+                    ans++;
+                    dfs(i,j,vis,grid);
                 }
             }
         }
