832ce95e01a9ecaab9098f70470bf72da77452e5
kushagrax2005 <kushagra14official@gmail.com>
2026-09-05 23:16:05 +0530

    Time: 2 ms (18.43%), Space: 9 MB (22.55%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    void f(int i,int k,int n,int &ans,vector<int>&d,vector<int>&nums,vector<vector<int>>&vec)
+    void f(int i,int k,int n,int &ans,vector<int>&d,vector<vector<int>>&vec)
     {
         if(d.size()==k)
         {
             if(ans==n)vec.push_back(d);
             return ;
         }
-        if(ans>n || i>=nums.size())return ;
-        ans+=nums[i];
-        d.push_back(nums[i]);
-        f(i+1,k,n,ans,d,nums,vec);
-        ans-=nums[i];
+        if(ans>n || i>9)return ;
+        ans+=i;
+        d.push_back(i);
+        f(i+1,k,n,ans,d,vec);
+        ans-=i;
         d.pop_back(); 
-        f(i+1,k,n,ans,d,nums,vec);
+        f(i+1,k,n,ans,d,vec);
     }
     vector<vector<int>> combinationSum3(int k, int n) {
         if(n>45)return {};
-        vector<int>nums;
+        // vector<int>nums;
         vector<int>d;
         int ans=0;
         vector<vector<int>>vec;
-        for(int i=1;i<=9;i++)
-        {
-            nums.push_back(i);
-        }
-        f(0,k,n,ans,d,nums,vec);
+        // for(int i=1;i<=9;i++)
+        // {
+        //     nums.push_back(i);
+        // }
+        f(1,k,n,ans,d,vec);
         return vec;
     }
 };
