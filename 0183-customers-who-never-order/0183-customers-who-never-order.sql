772af260d9a2d9295f302754bf7652537ff5d055
kushagrax2005 <kushagra14official@gmail.com>
2026-09-12 12:02:12 +0530

    Time: 205 ms (79.04%), Space: 0B (100%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,9 +1,6 @@
 -- Write your PostgreSQL query statement below
 select c1.name as Customers
 from customers c1
-where id not in(
-select c2.id
-from customers c2
-inner join orders o
-on c2.id=o.customerID
-)
+left join orders o
+on c1.id=o.customerID
+where o.customerID is NULL
